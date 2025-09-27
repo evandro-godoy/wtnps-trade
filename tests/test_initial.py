7c8ba9a2876904a70de9ebf7cd1625f7798a9fe6
evandro-godoy <godoy@whatsneps.com.br>
2025-09-27 16:02:54 -0300

    feat: setup initial project structure with poetry and pytest

---
+++ tests/test_initial.py
@@ -0,0 +1,8 @@
+# tests/test_initial.py
+import pytest
+
+def test_environment_setup():
+    """
+    Um teste simples para garantir que o pytest está configurado e funcionando.
+    """
+    assert True
